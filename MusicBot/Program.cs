6f50817a3eef18db5697526769cf2c422dc9dff9
kolik95 <kolik9594@gmail.com>
2018-06-23 18:49:52 +0200

    Optimized the Main method

--- MusicBot/Program.cs
+++ MusicBot/Program.cs
@@ -6,21 +6,13 @@ using Discord.WebSocket;
 namespace MusicBot
 {
     public class Program
-    {
-        private DiscordSocketClient _client;
-
-        private CommandHandler _handler;
-
-        private static void Main(string[] args)
-        {
-            new Program().StartAsync().GetAwaiter().GetResult();
-        }
-
-        private async Task StartAsync()
-        {
+    {      
+        private static async Task Main(string[] args)
+        {                       
+            
             if (string.IsNullOrEmpty(Config.Bot.Token)) return;
 
-            _client = new DiscordSocketClient(new DiscordSocketConfig
+            DiscordSocketClient _client = new DiscordSocketClient(new DiscordSocketConfig
             {
                 LogLevel = LogSeverity.Verbose
             });
@@ -31,12 +23,13 @@ namespace MusicBot
 
             await _client.StartAsync();
 
-            _handler = new CommandHandler(_client);
+            CommandHandler _handler = new CommandHandler(_client);
 
             await Task.Delay(-1);
+            
         }
 
-        private async Task Log(LogMessage msg)
+        private static async Task Log(LogMessage msg)
         {
             await Console.Out.WriteLineAsync(msg.Message);
         }
