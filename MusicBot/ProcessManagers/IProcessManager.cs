953b91eefb9fd719403a97c32d52c08e3d25a606
Jan Novák <enrich.silen@gmail.com>
2018-09-22 22:13:34 +0200

    Interface for Procces Manager

---
+++ MusicBot/ProcessManagers/IProcessManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace MusicBot.ProcessManagers
+{
+    interface IProcessManager
+    {
+        Task Suspend(Process process);
+        Task Resume(Process process);
+    }
+}
