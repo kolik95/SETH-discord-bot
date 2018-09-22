be50a940258ee04475243121255560a6f6b16f16
kolik95 <kolik9594@gmail.com>
2018-09-22 20:52:49 +0200

    Added AdioBridge

---
+++ MusicBot/Services/AudioBridge.cs
@@ -0,0 +1,41 @@
+﻿using Discord;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicBot
+{
+    class AudioBridge
+    {
+
+		private AudioService _audioService { get; set; }
+
+		private MessageService _messageService { get; set; }
+
+		public AudioBridge()
+		{
+
+			_audioService = new AudioService();
+			_messageService = new MessageService();
+
+		}
+
+		public async Task Play(string link, IVoiceChannel voice, IGuild guild, IMessageChannel channel, string username)
+		{
+
+			var _audioclient = _audioService.CheckForActiveChannel(guild);
+
+			if (_audioclient == null)
+				_audioclient = await _audioService.Join(guild, voice, channel);
+
+			_audioService.AddToQueue(link, guild, channel).Wait();
+
+			_audioService.CheckActivity(guild, channel, username);
+
+			_audioService.PlayQueue(guild, channel);
+
+		}
+
+	}
+}
